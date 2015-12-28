b2d668ddd026d6da14df3de4543507d7df9ac46a
Lukas <lukas_adrian.haack@mailbox.tu-dresden.de>
2015-12-28 17:11:42 +0100

    DataInitializer updated
    did not compile due to expired stock items being initialized by default

--- app/src/main/java/org/tudresden/ecatering/ECateringDataInitializer.java
+++ app/src/main/java/org/tudresden/ecatering/ECateringDataInitializer.java
@@ -112,8 +112,8 @@ private void initializeStock() {
 
 		
 		//Sahne im Stock verfügbar
-		stockManager.saveStockItem(stockManager.createStockItem(sahne, 0.525, LocalDate.of(2015, 12, 30)));
-		stockManager.saveStockItem(stockManager.createStockItem(sahne, 1.325, LocalDate.of(2015, 12, 24)));
+		stockManager.saveStockItem(stockManager.createStockItem(sahne, 0.525, LocalDate.of(2016, 12, 30)));
+		stockManager.saveStockItem(stockManager.createStockItem(sahne, 1.325, LocalDate.of(2016, 12, 24)));
 
 		
 	}
